24e40099b31d29c2c65bf90d252e5a038a0e2430
Icosa <117495728+H4drian@users.noreply.github.com>
2025-12-10 00:49:40 -0500

    fix(Main.java): placeholder print program

--- Main.java
+++ Main.java
@@ -4,8 +4,13 @@
  * Copyright (c) 2025, LJC
  */
 
+/*
+ * NOTE: Original code written around Java SE 8. Updated code written around Java SE 25 since 
+ * it isn't for a class which restricts version.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo World!");
+        System.out.println("");
     }
 }
