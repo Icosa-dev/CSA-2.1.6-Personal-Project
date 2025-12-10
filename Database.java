574455e858fdad3ecf3ea9f7a675c3447e6e5bc9
Icosa <117495728+H4drian@users.noreply.github.com>
2025-12-10 00:50:56 -0500

    fix(Database.java)

--- Database.java
+++ Database.java
@@ -14,10 +14,11 @@ import java.io.File;
  * @since 1.0
  */
 public class Database {
-    // Private attributes
-    File dbFile;
-    boolean verboseOutput;
+    // Private Attributes
+    private File dbFile;
+    private boolean verboseOutput;
 
+    // Public methods 
     /**
      * Database object constructor.
      * 
@@ -26,7 +27,46 @@ public class Database {
      */
     Database(String dbName, boolean enableVerboseOutput)
     {
+        logSuccess("TEST LOG SUCCESS");
+        logInfo("TEST LOG INFO");
+        logError("TEST LOG ERROR");
         this.dbFile = new File(dbName + ".cvs");
         this.verboseOutput = enableVerboseOutput;
+    }
+
+    /**
+     * Creates a key in the database file or, if exists, will updated the existing key.
+     * 
+     * @param key The name of the key to create.
+     * @param val The double value to assign to the key.
+     */
+    public void create(String key, double val) { }
+    
+    // Private Methods
+
+    // Logger methods
+    // Source - https://stackoverflow.com/a
+    // Posted by WhiteFang34, modified by community. See post 'Timeline' for change
+    // history
+    // Retrieved 2025-12-03, License - CC BY-SA 3.0
+    // ANSI Escape Codes for Colors e
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+
+    private void logSuccess(String msg) {
+        if (verboseOutput)
+            System.out.println("[" + ANSI_GREEN + "SUCCESS" + ANSI_RESET + "] " + msg);
+    }
+
+    private void logInfo(String msg) {
+        if (verboseOutput) 
+            System.out.println("[" + ANSI_YELLOW + "INFO" + ANSI_RESET + "] " + msg);
+    }
+
+    private void logError(String msg) {
+        if (verboseOutput)
+            System.err.println("[" + ANSI_RED + "ERROR" + ANSI_RESET + "] " + msg);
     }
 }
