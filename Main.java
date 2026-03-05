a4b4677fa2e6c9847936779552d5e7bd129c753e
Icosa <117495728+Icosa-dev@users.noreply.github.com>
2026-03-05 18:01:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,4 @@
 /*
- * SPDX-License-Identifer: BSD-2-Clause
- *
- * Copyright (c) 2025, LJC
- */
-
-/*
  * NOTE: Original code written around Java SE 8. Updated code written around Java SE 25 since 
  * it isn't for a class which restricts version.
  */
